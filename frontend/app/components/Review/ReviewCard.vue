68a0f840751d5a4fd68f90e8270e55df2705a1e2
Barna Bence <barnabenceneu@gmail.com>
2026-03-30 09:02:16 +0200

    ReviewCards made responsive

--- frontend/app/components/Review/ReviewCard.vue
+++ frontend/app/components/Review/ReviewCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-white rounded-xl shadow-md p-4 flex gap-3 items-start">
-    <div class="w-10 h-10 rounded-full bg-purple-500 flex items-center justify-center text-white font-bold overflow-hidden">
+    <div class="w-8 h-8 md:h-10 md:w-10 lg:h-12 lg:w-12 rounded-full bg-purple-500 flex items-center justify-center text-white font-bold overflow-hidden">
       <span v-if="!review.profile_image">
         {{ review.name.charAt(0) }}
       </span>
@@ -14,11 +14,11 @@
 
     <div class="flex-1">
       <div class="flex justify-between items-center">
-        <h4 class="font-semibold text-sm">
+        <h4 class="font-semibold text-md md:text-lg lg:text-xl">
           {{ review.name }}
         </h4>
 
-        <div class="flex text-yellow-400 text-sm">
+        <div class="flex text-yellow-400 text-md md:text-lg lg:text-xl">
           <span v-for="i in 5" :key="i">
             <span v-if="i <= review.rating">★</span>
             <span v-else class="text-gray-300">★</span>
@@ -26,7 +26,7 @@
         </div>
       </div>
 
-      <p class="text-xs text-gray-600 mt-2 line-clamp-3">
+      <p class="text-md md:text-lg text-gray-600 mt-2 line-clamp-3">
         {{ review.comment }}
       </p>
     </div>
