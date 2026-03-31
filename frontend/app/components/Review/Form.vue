ac96392ccce451f259db9459087fb065340407d1
Barna Bence <barnabenceneu@gmail.com>
2026-03-31 17:16:04 +0200

    Review post button styling finished

--- frontend/app/components/Review/Form.vue
+++ frontend/app/components/Review/Form.vue
@@ -7,7 +7,7 @@
           v-model="comment"
           id="review-comment"
           name="comment"
-          class="block w-full rounded-md border-gray-300 shadow-sm focus:border-purple-500 focus:ring-purple-500 sm:text-sm h-24 md:h-48"
+          class="block w-full rounded-md border-gray-300 shadow-md focus:border-purple-500 focus:ring-purple-500 sm:text-sm h-24 md:h-48 p-3 border border-gray-200"
           placeholder="Oszd meg az élményed..."
           required
         ></textarea>
@@ -16,9 +16,9 @@
     <div>
       <button
         type="submit"
-        class="inline-flex w-full justify-center rounded-md border border-transparent bg-purple-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2"
+        class="inline-flex w-full justify-center rounded-md border bg-[#36082A] px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-opacity-95"
       >
-        Értékelés hozzáadása
+        Értékelés közzététele
       </button>
     </div>
   </form>
