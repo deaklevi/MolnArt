94c5db73c69d13f1b829840a711e0de5b271c376
Barna Bence <barnabenceneu@gmail.com>
2026-04-08 19:56:10 +0200

    Product cards made smaller

--- admin/app/components/AppointmentModal.vue
+++ admin/app/components/AppointmentModal.vue
@@ -24,15 +24,30 @@ const form = reactive({
 
 const products = ref([...(props.appointment.products ?? [])])
 
-console.log(products)
+
 const submit = () => emit('save', { ...form })
+
+let selection = ref(0);
+function handleSelect(number: number){
+  selection.value = number;
+  console.log(selection);
+}
 </script>
 
-<template>
-  <div class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-    <div class="bg-white rounded-xl shadow-xl w-full max-w-lg p-6 space-y-4">
+<template >
 
-      <div class="flex justify-between items-center">
+  <div class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+    
+    <div class="bg-white rounded-xl shadow-xl w-full max-w-lg min-h-[75%] p-6 space-y-4">
+      
+      <div class="flex flex-row justify-around pb-4">
+        <h2 @click="handleSelect(0)">Időpont</h2>
+        <h2 @click="handleSelect(1)">Foglalhatóság</h2>
+        <h2 @click="handleSelect(2)">Egyéb elfoglaltság</h2>
+      </div>
+
+    <div id="appointment" v-if="selection=== 0">
+        <div class="flex justify-between items-center">
         <h2 class="text-lg font-semibold">
           {{ isNew ? 'Időpont hozzáadása' : 'Időpont szerkesztés' }}
         </h2>
@@ -40,15 +55,19 @@ const submit = () => emit('save', { ...form })
       </div>
 
       <div class="space-y-3">
-        <input v-model="form.service" placeholder="Service"
+        <div class="grid grid-cols-2 gap-3">
+          <input v-model="form.name" placeholder="Customer name"
           class="w-full border rounded-lg px-3 py-2 text-sm" />
-        <input v-model="form.name" placeholder="Customer name"
+          <input v-model="form.phone_number" placeholder="Phone"
           class="w-full border rounded-lg px-3 py-2 text-sm" />
-        <input v-model="form.email" placeholder="Email"
+        </div>
+
+          <input v-model="form.email" placeholder="Email"
           class="w-full border rounded-lg px-3 py-2 text-sm" />
-        <input v-model="form.phone_number" placeholder="Phone"
+          <input v-model="form.service" placeholder="Service"
           class="w-full border rounded-lg px-3 py-2 text-sm" />
 
+        
         <div class="grid grid-cols-2 gap-2">
           <div>
             <label class="text-xs text-gray-500">Tól</label>
@@ -61,10 +80,11 @@ const submit = () => emit('save', { ...form })
               class="w-full border rounded-lg px-3 py-2 text-sm" />
           </div>
         </div>
-      </div>
 
+      </div>
+      
       <!-- products -->
-      <h2 class="pt-2 text-md font-semibold">Használt termékek</h2>
+      <h2 class="pt-2 text-md font-semibold my-3">Használt termékek</h2>
       <div class="h-[15rem] flex flex-col gap-2 overflow-y-auto">
         <AppointmentProductCard  
           v-for="(product,i) in products"
@@ -75,11 +95,17 @@ const submit = () => emit('save', { ...form })
           class="mb-2"
           
         />
-        
+                <AppointmentProductCard  
+          v-for="(product,i) in products"
+          :key="product.id"
+          :product="product"
+          @update:quantity="products[i].quantity = $event"
+          @remove="products.splice(i, 1)"
+          class="mb-2"
+          
+        />
+
       </div>
-
-
-      <!-- gombok -->
       <div class="flex justify-between pt-5">
         <button v-if="!isNew"
           @click="emit('delete', appointment.id)"
@@ -91,6 +117,16 @@ const submit = () => emit('save', { ...form })
           {{ isNew ? 'Foglalás' : 'Mentés' }}
         </button>
       </div>
+
+    </div>
+      
+    <div v-if="selection === 1">
+      <h2 class="text-lg font-semibold">Állitsd be mettől meddig lehet foglalni</h2>
+
+
+      
+    </div>
+
 
     </div>
   </div>
