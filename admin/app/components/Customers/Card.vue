27e975a80ee85bd0cc4c0732d3ca50d8df19bd13
Barna Bence <barnabenceneu@gmail.com>
2026-04-21 20:39:16 +0200

    CustomerPage cards

---
+++ admin/app/components/Customers/Card.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    customer:Object,
+})
+
+</script>
+
+<template>
+    <div class="bg-white shadow-md rounded-2xl p-4 border max-w-md" id="card">
+      <h2 class="font-bold text-xl mb-2">{{ customer.name }}</h2>
+      <p class="font-semibold">{{ customer.email }}</p>
+      <p class="italic">{{ customer.phone_number }}</p>
+    </div>
+</template>
+
